ecb58a1f86bb4d5cc306208bfe15438ab334ccfe
Ty <nt85519@cathaybk.com.tw>
2022-08-23 17:43:27 +0800

    上上去回家用

--- src/app/shared/shared/shared.module.ts
+++ src/app/shared/shared/shared.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const modules: any[] = [CommonModule, RouterModule];
 
+const MATERIAL_MODULES = [MatToolbarModule];
+
 @NgModule({
   declarations: [],
-  imports: [...modules],
-  exports: [...modules],
+  imports: [...modules, ...MATERIAL_MODULES],
+  exports: [...modules, ...MATERIAL_MODULES],
 })
 export class SharedModule {}
