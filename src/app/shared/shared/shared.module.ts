62a646dfe451f33a6414ba34edd49601ab2fc9f8
Ty <nt85519@cathaybk.com.tw>
2022-08-24 10:34:32 +0800

    header & footer

--- src/app/shared/shared/shared.module.ts
+++ src/app/shared/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 const modules: any[] = [CommonModule, RouterModule];
 
-const MATERIAL_MODULES = [MatToolbarModule];
+const MATERIAL_MODULES = [MatToolbarModule, MatIconModule];
 
 @NgModule({
   declarations: [],
