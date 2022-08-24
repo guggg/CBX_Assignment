4872a858b6a0a803ba3dca6efc97350e1a7211a3
Ty <king138786@gmail.com>
2022-08-24 22:49:18 +0800

    我不知道我做了三小

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,36 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LayoutModule } from './layout/layout.module';
-import { HomeComponent } from './modules/home/home.component';
-import { PagenotfoundComponent } from './modules/pagenotfound/pagenotfound.component';
-import { SharedModule } from './shared/shared/shared.module';
-import { BREAKPOINT } from '@angular/flex-layout';
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LayoutModule } from "./layout/layout.module";
+import { HomeComponent } from "./modules/home/home.component";
+import { PagenotfoundComponent } from "./modules/pagenotfound/pagenotfound.component";
+import { SharedModule } from "./shared/shared.module";
+import { BREAKPOINT } from "@angular/flex-layout";
 
 const COMPONENTS: any[] = [AppComponent, HomeComponent, PagenotfoundComponent];
 
-const PRINT_BREAKPOINTS = [{
-  alias: 'xs.print',
-  suffix: 'XsPrint',
-  mediaQuery: 'print and (max-width: 297px)',
-  overlapping: false,
-  priority: 1001 // Needed if overriding the default print breakpoint
-}];
+const PRINT_BREAKPOINTS = [
+  {
+    alias: "sm",
+    mediaQuery: "screen and (max-width: 768px)",
+    overlapping: false,
+    priority: 1001,
+  },
+  {
+    alias: "lg",
+    mediaQuery: "screen and (max-width: 1280px)",
+    overlapping: false,
+    priority: 1001,
+  },
+];
 
 @NgModule({
   declarations: [...COMPONENTS],
   imports: [SharedModule, AppRoutingModule, NoopAnimationsModule, LayoutModule],
   providers: [
-    { provide: BREAKPOINT, useValue: PRINT_BREAKPOINTS, multi: true },
+    // { provide: BREAKPOINT, useValue: PRINT_BREAKPOINTS, multi: true },
   ],
   bootstrap: [AppComponent],
 })
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { SharedModule } from './../shared/shared/shared.module';
+import { SharedModule } from '../shared/shared.module';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { LayoutComponent } from './layout.component';
